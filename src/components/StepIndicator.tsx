42687bad86594fb59b9b78bbdac49006bc6eae5a
RAVI <raviqs@yahoo.com>
2026-04-18 17:45:37 +0530

    Add files via upload

--- src/components/StepIndicator.tsx
+++ src/components/StepIndicator.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { CheckCircle2, Circle } from 'lucide-react';
+import { CheckCircle2 } from 'lucide-react';
 
 interface StepIndicatorProps {
   currentStep: number;
